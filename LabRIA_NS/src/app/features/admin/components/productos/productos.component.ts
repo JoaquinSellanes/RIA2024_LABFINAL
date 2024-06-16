9cfc6953b9ebce7e13c982bfe3977cd8d59812fd
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2024-06-16 19:09:22 -0300

    a

--- LabRIA_NS/src/app/features/admin/components/productos/productos.component.ts
+++ LabRIA_NS/src/app/features/admin/components/productos/productos.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../../shared/models/producto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.scss'
+  styleUrl: './productos.component.scss',
+  providers: [ProductoService, HttpClient]
 })
-export class ProductosComponent {
-  productos = [[], [], [], [], [], [], [], [], [], [], [], []];
+export class ProductosComponent implements OnInit {
+  productos = [];
 
+  constructor(private productoService: ProductoService) { }
+
+  async ngOnInit() {
+    await this.productoService.getProductos().then((data) => {
+      this.productos = data;
+    }).catch((error) => {
+      console.log(error);
+    });
+
+    console.log(this.productos);
+  }
 }
--- LabRIA_NS/src/app/features/admin/services/producto.service.ts
+++ LabRIA_NS/src/app/features/admin/services/producto.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Producto } from '../../shared/models/producto';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  apiUrl = environment.apiUrl;
 
   constructor() { }
 
   async createProducto(producto: Producto) {
     console.log("service -> ", producto);
+  }
+
+  // traer productos /productos/disponibles
+  async getProductos() {
+    try {
+      const response = await fetch(`${this.apiUrl}/productos`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 }
