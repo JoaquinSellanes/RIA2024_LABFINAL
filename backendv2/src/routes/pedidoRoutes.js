08dd915b45fea483258ff3c59fb073d4b907a43f
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2024-06-21 20:47:50 -0300

    Update pedidoRoutes.js

--- backendv2/src/routes/pedidoRoutes.js
+++ backendv2/src/routes/pedidoRoutes.js
@@ -4,8 +4,8 @@ const pedidoController = require('../controllers/pedidoController');
 
 router.post('/', pedidoController.crearPedido);
 router.get('/:id', pedidoController.obtenerPedidoPorId);
-router.post('/all', pedidoController.obtenerTodosLosPedidos);
-router.post('/ingredientes', pedidoController.calcularIngredientesTotales);  // Nueva ruta para calcular ingredientes totales
+router.get('/all', pedidoController.obtenerTodosLosPedidos);
+router.get('/ingredientes', pedidoController.calcularIngredientesTotales);  // Nueva ruta para calcular ingredientes totales
 // router.post('/', pedidoController.crearPedido);
 // router.get('/:id', pedidoController.obtenerPedidoPorId);
 // router.put('/:id', pedidoController.actualizarPedido);
