9ec66d8168e67948d19559e2696f5d876b85b516
Ezequiel <exzeblan@gmail.com>
2024-06-27 17:52:22 -0300

    miCuenta

--- backendv2/src/routes/usuarioRoutes.js
+++ backendv2/src/routes/usuarioRoutes.js
@@ -5,6 +5,8 @@ const usuarioController = require('../controllers/usuarioController');
 
 router.get('/',                 verifyToken, isAdmin, usuarioController.listarUsuarios);
 router.post('/modificar-rol',   verifyToken, isAdmin, usuarioController.modificarRolUsuario);
+router.get('/mi-cuenta',        verifyToken, usuarioController.obtenerMiCuenta);
+
 
 module.exports = {
     route: '/usuarios',
