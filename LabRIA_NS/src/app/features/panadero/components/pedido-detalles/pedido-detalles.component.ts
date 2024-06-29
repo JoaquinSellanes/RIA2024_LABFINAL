ba0c13f7e8cf035e31d41cae65dcc05b83f67cd9
Ezequiel <exzeblan@gmail.com>
2024-06-29 01:12:59 -0300

    tamo activo

--- LabRIA_NS/src/app/features/panadero/components/pedido-detalles/pedido-detalles.component.ts
+++ LabRIA_NS/src/app/features/panadero/components/pedido-detalles/pedido-detalles.component.ts
@@ -10,6 +10,7 @@ import { PedidosService } from '../../services/pedidos.service';
 export class PedidoDetallesComponent implements OnInit {
   @Input() pedidoId!: number;
   pedido: any;
+  insumosTotales: { nombre: string; totalCantidad: number }[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -22,15 +23,33 @@ export class PedidoDetallesComponent implements OnInit {
     try {
       const response = await this.pedidosService.getPedido(this.pedidoId);
       this.pedido = response;
-      this.pedido.productos.forEach((item: any) => {
-        item.producto.ingredientes.forEach((ingrediente: any) => {
-          ingrediente.totalCantidad = ingrediente.cantidad * item.cantidad;
-        });
-      });
+      this.calcularInsumosTotales();
       console.log("Pedido", this.pedido);
+      console.log("Insumos Totales", this.insumosTotales);
     } catch (error) {
       console.error("Error fetching pedido details:", error);
     }
+  }
+
+  calcularInsumosTotales() {
+    const insumosMap: { [key: string]: number } = {};
+
+    this.pedido.productos.forEach((item: any) => {
+      item.producto.ingredientes.forEach((ingrediente: any) => {
+        ingrediente.totalCantidad = ingrediente.cantidad * item.cantidad; // Calcular totalCantidad para cada ingrediente
+        const cantidadTotal = ingrediente.totalCantidad;
+        if (insumosMap[ingrediente.nombre]) {
+          insumosMap[ingrediente.nombre] += cantidadTotal;
+        } else {
+          insumosMap[ingrediente.nombre] = cantidadTotal;
+        }
+      });
+    });
+
+    this.insumosTotales = Object.keys(insumosMap).map(nombre => ({
+      nombre,
+      totalCantidad: insumosMap[nombre]
+    }));
   }
 
   volver() {
